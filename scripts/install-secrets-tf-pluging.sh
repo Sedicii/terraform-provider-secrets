24e3b5ac84e300fb458c812c256040fdebb3ff53
Sergio Medina <lumasepa@gmail.com>
2018-02-23 09:05:15 +0000

    Update install script

--- scripts/install-secrets-tf-pluging.sh
+++ scripts/install-secrets-tf-pluging.sh
@@ -3,7 +3,21 @@
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 VERSION="v0.1.0"
 PROVIDER_DOWNLOAD_URL="https://github.com/Sedicii/terraform-provider-secrets/releases/download/${VERSION}/terraform-provider-secrets_${OS}-amd64_${VERSION}"
+CLI_DOWNLOAD_URL="https://github.com/Sedicii/terraform-provider-secrets/releases/download/${VERSION}/tf-secrets_${OS}-amd64_${VERSION}"
 TF_PLUGINS_PATH="${HOME}/.terraform.d/plugins/${OS}_amd64/"
-
+PLUGIN_DEST="${TF_PLUGINS_PATH}/terraform-provider-secrets_${VERSION}"
 mkdir -p "${TF_PLUGINS_PATH}"
-curl ${PROVIDER_DOWNLOAD_URL} --output "${TF_PLUGINS_PATH}/terraform-provider-secrets_${VERSION}"
+
+curl -L ${PROVIDER_DOWNLOAD_URL} --output ${PLUGIN_DEST}
+chmod +x ${PLUGIN_DEST}
+
+CLI_DEST="/usr/local/bin/tf-secrets"
+
+if [ "${OS}" == "darwin" ]; then
+    curl -L ${CLI_DOWNLOAD_URL} --output ${CLI_DEST}
+    chmod +x ${CLI_DEST}
+else
+    sudo curl -L ${CLI_DOWNLOAD_URL} --output ${CLI_DEST}
+    sudo chmod 755 ${CLI_DEST}
+fi
+
